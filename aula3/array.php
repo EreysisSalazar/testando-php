2a8282595b34f435cdcd7a27c8a6541e3d67a05b
Ereysis Salazar <134754969+EreysisSalazar@users.noreply.github.com>
2023-08-25 13:47:34 -0300

    Update array.php

--- aula3/array.php
+++ aula3/array.php
@@ -1,23 +1,21 @@
 <?php
-echo "<h1>array indexado</h1>";
-$carros =array("Palio","Marea","Fusca","Kombi");
+echo "<h1>Array Indexado</h1>";
+$carros =array("Palio","Marea","Fusca","kombi");
 
 //echo $carros[2];
 $i=0;
 while ($i<4){
     echo $carros[$i]."<br>";
     $i++;
+    
 
 }
-
 echo "<hr>";
 for($x =0;$x<4;$x++){
     echo $carros[$x]."<br>";
-
 }
 
 echo "<hr>";
-foreach ($carros as $carros) {
-   echo $carros."<br>";
-
+foreach($carros as $carros){
+    echo $carros."<br>";
 }
