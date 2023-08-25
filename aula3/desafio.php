770c61865b6a09ce72687a767588769384c1d1d3
Ereysis Salazar <134754969+EreysisSalazar@users.noreply.github.com>
2023-08-25 13:49:53 -0300

    Update desafio.php

--- aula3/desafio.php
+++ aula3/desafio.php
@@ -1,10 +1,11 @@
-<?php
-echo "h1array asociativo</h1>";
-$idade = array("adriana"=>matematica, "Danda"=>quimica,"Veri"=>portugues, "Cris"=>25);
-
+?php
+echo "<h1Array Associativo</h1>";
+$instrutores= array("Adriana=>matematica","Danda=>portugues","Veri=>ed fisica","cris=>logistica");
 echo "<hr>";
-foreach ($idade as $x => $x_value) {
-   echo "key = ".$x. ",value" .$x_value;
-   echo "<br>";
-
+foreach ($instrutores as $x =>$x_value){
+    echo "Key = ".$x.",intrutore
+    
+    
+    =" .$x_value;
+    echo "<br>";
 }
